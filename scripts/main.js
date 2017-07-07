e21c2ffd3167128ddaa7f9fc4daab76fdc11b58d
Kelton Crouse <kelton.crouse@gmail.com>
2017-07-06 23:45:42 -0400

    Set usestoredname as function, formatting cleaned up

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,36 @@
 var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/penguin.jpg') {
-      myImage.setAttribute ('src','images/penguin2.jpg');
-    } else {
-      myImage.setAttribute ('src','images/penguin.jpg');
-    }
-}
-
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
+
+usestoredName();
 
 myButton.onclick = function() {
   setUserName();
 }
 
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'This is Merp the penguin, ' + myName;
+
+
+
+myImage.onclick = function() {
+      var mySrc = myImage.getAttribute('src');
+      if(mySrc === 'images/penguin.jpg') {
+        myImage.setAttribute ('src','images/penguin2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/penguin.jpg');
+    }
 }
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'This is Merp the penguin, ' + storedName;
+function setUserName() {
+      var myName = prompt('Please enter your name.');
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'This is Merp the penguin, ' + myName;
+}
+
+function usestoredName() {
+      if(!localStorage.getItem('name')) {
+      setUserName();
+    } else {
+      var storedName = localStorage.getItem('name');
+      myHeading.textContent = 'This is Merp the penguin, ' + storedName;
+    }
 }
